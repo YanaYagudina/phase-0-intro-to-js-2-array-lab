a9f15f9c9d8ce086c9867d9a7ff1039c4aff8fc2
Yana <yana.videoeditor.gmail.com>
2022-08-19 19:08:48 -0600

    commit tree a6de25b7d258de64d8f2d9a91fa3815875a14f9a

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(X) {
+    cats.push(X)
+}
+function destructivelyPrependCat(X) {
+    cats.unshift(X)
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+
+}
+function appendCat(x) {
+    const copyOfCats = cats.slice();
+    copyOfCats.push(x);
+    return copyOfCats;
+}
+function prependCat(x) {
+    const copyOfCats = cats.slice();
+    copyOfCats.unshift(x);
+    return copyOfCats;
+}
+
+function removeLastCat() {
+    const copyOfCats = [1, ...cats, 1]
+    return copyOfCats.slice(1, copyOfCats.length - 2)
+        
+}
+    
+function removeFirstCat() {
+    const copyOfCats = [...cats]
+    copyOfCats.shift()
+    return copyOfCats
+}
